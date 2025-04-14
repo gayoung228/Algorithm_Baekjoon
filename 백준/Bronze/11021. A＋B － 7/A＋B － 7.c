1408f88fd157dc939a034434ffdcd030bd47cc95
gayoung228 <103436798+gayoung228@users.noreply.github.com>
2025-04-14 23:36:44 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    int num, a, b, i;
+
+    scanf("%d", &num);
+
+    for (i = 1; i <= num; i++) {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d\n", i, a + b);
+    }
+
+    return 0;
+}
