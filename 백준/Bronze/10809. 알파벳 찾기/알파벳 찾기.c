2c5e3fcfc44b71f4a76ab913614dfdad13f084e2
이가영 <103436798+gayoung228@users.noreply.github.com>
2025-07-16 17:21:52 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    int alphabet[26], len, index;
+
+    for (int i = 0; i < 26; i++) {
+        alphabet[i] = -1; 
+    }
+
+    scanf("%s", s);
+
+    len = strlen(s);
+    for (int i = 0; i < len; i++) {
+        index = s[i] - 'a'; // 'b' - 'a' -> 98-97 = 1
+        if (alphabet[index] == -1) {
+            alphabet[index] = i;
+        }
+        else {
+            continue;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        printf("%d ", alphabet[i]);
+    }
+
+    return 0;
+}
