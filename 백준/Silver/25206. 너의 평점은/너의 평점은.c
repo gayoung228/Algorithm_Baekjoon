d205809039d16a859d1126abe8d66aede4fed90e
이가영 <103436798+gayoung228@users.noreply.github.com>
2025-07-17 14:03:17 +0900

    [Silver V] Title: 너의 평점은, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    double score;
+    char subject[50];
+    char major[2];
+
+    double ans=0.0, sumScore=0.0, majorScore = 0.0;
+
+    for (int i = 0; i < 20; i++) {
+        scanf("%s %lf %s", subject, &score, major);
+
+        if (major[0] == 'P') {
+            continue;
+        }
+        sumScore += score;
+        if (major[0] == 'F') {
+            continue;
+        }
+        else if (major[0] == 'A') {
+            majorScore = 4;
+        }
+        else if (major[0] == 'B') {
+            majorScore = 3;
+        }
+        else if (major[0] == 'C') {
+            majorScore = 2;
+        }
+        else {
+            majorScore = 1;
+        }
+        if (major[1] == '+') {
+            majorScore += 0.5;
+        }
+        ans += score * majorScore;
+    }
+
+    printf("%lf", ans / sumScore);
+
+    return 0;
+}
