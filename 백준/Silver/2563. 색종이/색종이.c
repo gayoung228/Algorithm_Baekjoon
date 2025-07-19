f0a9bea69b6033aa2ddc4dd654d41dfa9fdf767b
이가영 <103436798+gayoung228@users.noreply.github.com>
2025-07-19 23:51:49 +0900

    Update 색종이.c

--- 백준/Silver/2563. 색종이/색종이.c
+++ 백준/Silver/2563. 색종이/색종이.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 int main() {
-	int paper[100][100] = { 0, };
-	int n, x, y, count = 0;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
+	int square[100][100] = { 0, };
+	int num, x, y, count = 0; //x는 열, y는 행
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++) {
 		scanf("%d %d", &x, &y);
-		for (int j = y; j < y + 10; j++) {	//흔히 알고있는 x좌표는 2차원 배열에서 열에 해당 y좌표는 2차원 배열에서 행에 해당
+		for (int j = y; j < y + 10; j++) {	
 			for (int k = x; k < x + 10; k++) {
-				paper[j][k] = 1;	
+				square[j][k] = 1;	
 			}
 		}
 	}
 		for (int j = 0; j < 100; j++) {
 			for (int k = 0; k < 100; k++) {
-				if (paper[j][k] == 1) {
+				if (square[j][k] == 1) {
 					count++; //어차피 1로 초기화된 것을 count하는것이기 때문에 중복은 없다.
 				}
 			}
